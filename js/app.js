046a7be55f43dafdba9bfa0bee9ab856ede02b9f
pelletmaeva <pelletmaeva@gmail.com>
2018-04-17 09:17:41 +0200

    feat: make Enemies and Player appear

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/Drone1_120X120.png';
+    // Take the position from the parameter
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,8 +27,12 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function(x, y) {
+  // Define the initial sprite to be used
   this.sprite = 'images/spaceship_rogue_120X120.png';
+  // Take the position from the parameter
+  this.x = x;
+  this.y = y;
 };
 
 Player.prototype.update = function(dt) {
@@ -39,12 +46,12 @@ Player.prototype.handleInput = function() {
 };
 
 // Now instantiate your objects.
-let player = new Player();
+let player = new Player(0, 0);
 
-let enemy1 = new Enemy();
-let enemy2 = new Enemy();
-let enemy3 = new Enemy();
-let enemy4 = new Enemy();
+let enemy1 = new Enemy(0, 0);
+let enemy2 = new Enemy(0, 0);
+let enemy3 = new Enemy(0, 0);
+let enemy4 = new Enemy(0, 0);
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [enemy1, enemy2, enemy3, enemy4];
 // Place the player object in a variable called player
